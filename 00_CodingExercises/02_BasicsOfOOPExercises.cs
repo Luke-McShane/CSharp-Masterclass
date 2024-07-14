96a3ad06dddd20d3271c51f522f06bda9ae0fe4e
Luke McShane <lukejsmcshane@gmail.com>
2024-07-14 14:02:38 +0100

    Introduced OOP

---
+++ 00_CodingExercises/02_BasicsOfOOPExercises.cs
@@ -0,0 +1,36 @@
+﻿namespace _00_CodingExercises;
+
+public class _02_BasicsOfOOPExercises
+{
+
+}
+
+
+/*
+An alternative method to procedural programming is object-oriented programming (OOP), which is a paradigm where objects containing data
+and methods play a central role.
+
+We can define what data and methods objects of some type will contain by defining classes.
+
+We can have any amount of objects (instances) of any class.
+
+
+
+Some of the issues potentially faced with procedural programming are as follows:
+1. We will be more likely to write spaghetti code (untidy, complicated, hard-to-follow code), which is an antipattern.
+2. We have no control over who can and cannot access methods, because no methods are encapsulated within methods with levels of protection.
+3. There is no separation by levels of abstraction, meaning code is much more difficult to maintain with changing requirements.
+4. Logic is not easily configurable due to the complexity of the code.
+
+Some benefits of OOP:
+1. Code is modular, making it easier to resuse, maintain, and modify.
+2. Code is flexible, meaning we can change its behaviour easily.
+3. Code is easier to understand.
+4. Code is less prone to error given how we can control how methods and data are accessed.
+
+There are 4 fundamentals of OOP:
+1. Inheritence.
+2. Polymorphism.
+3. Abstraction.
+4. Encapsulation.
+*/
