e1480b7c5f1e6c4b0d411cf3319cf6578689f58f
Luke McShane <lukejsmcshane@gmail.com>
2024-07-15 17:22:23 +0100

    Moved OOP basics file and developed code around constructor overloaded and passing objects as arguments

--- 00_CodingExercises/Program.cs
+++ 00_CodingExercises/Program.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿CSharpFundementalExercises.Entry();
 
-class Program
-{
-  public static void Main(string[] args)
-  {
-    CSharpFundementalExercises.Entry();
-  }
-}
 
+/*
+Below we have 9 lines of code that perform the same task as the single line of code above. This is because we have top-level statements
+enabled in our project. All code in C# must be contained withing a class, and top level statements remove the excess code by placing
+your program's entry point inside a static method within a class. This happens upon compilation and can only occur in the entry point
+of the application, nowhere else.
+
+If we define methods inside a file with top-level statements, they cannot have access modifiers, as they will by default only be accessible
+from within that file.
+*/
+
+// namespace _00_CodingExercises
+// {
+//   internal class Program
+//   {
+//     public static void Main(string[] args)
+//     {
+//       CSharpFundementalExercises.Entry();
+//     }
+//   }
+// }
 
--- 02_BasicsOfOOP/Program.cs
+++ 02_BasicsOfOOP/Program.cs
@@ -3,6 +3,11 @@ var myRec = new Rectangle();
 System.Console.WriteLine($"Width: {myRec.Width}\nHeight: {myRec.Height}");
 myRec.Width = 3;
 myRec.Height = 2;
+System.Console.WriteLine($"Width: {myRec.Width}\nHeight: {myRec.Height}");
+
+var appointment = new MedicalAppointment("Dave");
+appointment.Reschedule(8, 12);
+
 class Rectangle
 {
   // These variables are the fields of the Rectangle class.
@@ -13,8 +18,80 @@ class Rectangle
     Width = w;
     Height = h;
   }
+
+  // Names of methods should *always* start with a verb.
+  // The default access modifier for a field or method is always the *most restrictive* that is valid in a given context.
+  public int CalculateArea()
+  {
+    // An expression is something that evaluates to a value, such as this statment below.
+    return Width * Height;
+
+    // A statement is something that does *not* evaluate to a value, such as Console.Writeline("Hello world"); or an if-statement.
+  }
+
+  // Above we have a standard method, but we can create an expression-bodied method whenever we have a single line of code in the body.
+  // It can only be when we have a single line of code, and we use the '=>' arrow to define an expression-bodied method.
+  public int CalculateCircumference() => (Width * Height) * 2;
 }
 
+
+
+//Here we create a class that prints information from a MedicalAppointment object we pass it.
+// We can access the public method GetAppointment from the passed object, as shown below.
+class MedicalAppointmentPrinter
+{
+  public void Print(MedicalAppointment appointment) =>
+              Console.WriteLine($"Your appointment will now take place on: {appointment.GetAppointment()}");
+}
+
+class MedicalAppointment
+{
+  private string _patientName;
+  private DateTime _date;
+
+  public MedicalAppointment(string patientName, DateTime date)
+  {
+    _patientName = patientName;
+    _date = date;
+  }
+
+  // Here we call one constructor from another. The functionality in this constructor says "If the user doesn't give an appointment date,
+  // automatically set an appointment one week from now. If there were code in the constructor body, it would be executed after the 
+  // second constructor call. The 'this' keyword refers to the current instance of the class (the object).
+  public MedicalAppointment(string patientName) : this(patientName, 7)
+  {
+    // This code below is now redundant because we have overloaded the constructor and passed the appropriate arguments.
+    // _patientName = patientName;
+    // _date = DateTime.Now.AddDays(7);
+  }
+
+  public MedicalAppointment(string patientName, int daysFromNow)
+  {
+    _patientName = patientName;
+    _date = DateTime.Now.AddDays(daysFromNow);
+    System.Console.WriteLine($"Date of appointment: {_date}. Name: {_patientName}");
+  }
+
+  public DateTime GetAppointment() => _date;
+
+  // Overloading
+  // Here we set the first instance of the method, and below we set a second instance. We can do this because of the ability to overload
+  // methods and constructors in C#, meaning we can create methods of the same name, which may serve a similar function, but will be differentiated by their
+  // parameter list/signature.
+  public void Reschedule(DateTime date)
+  {
+    _date = date;
+    var printer = new MedicalAppointmentPrinter();
+    printer.Print(this);
+  }
+
+  public void Reschedule(int month, int day)
+  {
+    _date = new DateTime(_date.Year, month, day);
+    var printer = new MedicalAppointmentPrinter();
+    printer.Print(this);
+  }
+}
 /*
 An alternative method to procedural programming is object-oriented programming (OOP), which is a paradigm where objects containing data
 and methods play a central role.
@@ -44,6 +121,11 @@ There are 4 fundamentals of OOP:
     Abstraction hides certain properties and methods of a class that need not be accessed outside the class.
     Users should be given an interface to operate on the type, not everything within the type as much would be irrelevant to the user.
 4. Encapsulation.
+    Encapsulation is binding together the data and the methods that manipulate them in a single class.
+    If we have separate classes that directly manipulate fields of a class then we add unnecessary complexity to our program.
+    Also, we would have to decrease the restriction level of fields, which increases the risk to our program.
+    Data hiding is not the same as encapsulation. Encapsulation is the bundling of fields and methods into one class, whereas data hiding
+    is increasing the protection level of fields. Often encapsulation *enables* data hiding.
 
 An object may have fields, which are variables that specifically belong to that object of the class.
 Unlike standard variables, fields do not need to be initialised to have a value. When an object is created, unless the fields are initialised,
