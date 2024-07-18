754d0badbe7920e7e68f13420b3798617d412c01
Luke McShane <lukejsmcshane@gmail.com>
2024-07-18 14:50:51 +0100

    created a Global Usings file

---
+++ 02_NamesAfterRefactorToSRP/GlobalUsings.cs
@@ -0,0 +1,3 @@
+// This class is for all the libraries we want to use across the code. It is good practise to put all the global usings
+// in a single file. Now the System.Diagnostics class will be available in all files in this project.
+global using System.Diagnostics;
