badf864c2661595336b1fd317e2ef0d487f0397d
Luke McShane <lukejsmcshane@gmail.com>
2024-07-18 17:07:38 +0100

    initial coding structure. created basic validation and while loop.

--- 02_DiceRollGame/Program.cs
+++ 02_DiceRollGame/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries.");
+string? userGuess = Console.ReadLine();
+bool hasGuesses = true;
+
+while (hasGuesses)
+{
+  System.Console.WriteLine(ValidateInput.IsValid(userGuess));
+}
+class Dice
+{
+  private readonly int Number { get; }
+  private static readonly Random _rnd = new Random();
+
+  Dice()
+  {
+    Number = _rnd.Next(1, 6);
+  }
+}
+
+class ValidateInput
+{
+  public static bool IsValid(string? input) => int.TryParse(input, out _);
+}
