b8bc9e793d90486474fadc30ce26bb67c0e2f052
Luke McShane <lukejsmcshane@gmail.com>
2024-07-18 12:27:08 +0100

    First round of refactoring using the SRP and DRY principles

--- 02_NamesAfterRefactorToSRP/Program.cs
+++ 02_NamesAfterRefactorToSRP/Program.cs
@@ -4,12 +4,12 @@
 // the codebase will be easier to maintain, for aspects of the program will be split up appropriately/according to this principle.
 
 var names = new Names();
-var path = names.BuildFilePath();
+var path = BuildFilePath.Build();
 
 if (File.Exists(path))
 {
   Console.WriteLine("Names file already exists. Loading names.");
-  names.ReadFromTextFile();
+  StringRepository.Read(path);
 }
 else
 {
@@ -22,25 +22,28 @@ else
   names.AddName("123 definitely not a valid name");
 
   Console.WriteLine("Saving names to the file.");
-  names.WriteToTextFile();
+  StringRepository.Write(path, names.All);
 }
-Console.WriteLine(names.Format());
+
+Console.WriteLine(string.Join(Environment.NewLine, names));
 
 Console.ReadLine();
 
 public class Names
 {
-  private readonly List<string> _names = new List<string>();
-
+  public List<string> All { get; } = new List<string>();
   public void AddName(string name)
   {
-    if (IsValidName(name))
+    if (NameValidator.IsValid(name))
     {
-      _names.Add(name);
+      All.Add(name);
     }
   }
+}
 
-  private bool IsValidName(string name)
+static class NameValidator
+{
+  public static bool IsValid(string name)
   {
     return
         name.Length >= 2 &&
@@ -48,27 +51,27 @@ public class Names
         char.IsUpper(name[0]) &&
         name.All(char.IsLetter);
   }
+}
 
-  public void ReadFromTextFile()
+static class StringRepository
+{
+  private static readonly string Separator = Environment.NewLine;
+  static public void Read(string path)
   {
-    var fileContents = File.ReadAllText(BuildFilePath());
-    var namesFromFile = fileContents.Split(Environment.NewLine).ToList();
-    foreach (var name in namesFromFile)
-    {
-      AddName(name);
-    }
+    var fileContents = File.ReadAllText(path);
+    List<string> namesFromFile = fileContents.Split(Separator).ToList();
   }
 
-  public void WriteToTextFile() =>
-      File.WriteAllText(BuildFilePath(), Format());
+  static public void Write(string path, List<string> names) =>
+      File.WriteAllText(path, string.Join(Separator, names));
+}
 
-  public string BuildFilePath()
+static class BuildFilePath
+{
+  public static string Build()
   {
     //we could imagine this is much more complicated
     //for example that path is provided by the user and validated
     return "names.txt";
   }
-
-  public string Format() =>
-      string.Join(Environment.NewLine, _names);
 }
