e58b431fca6debe82b88584bacda620265cdf110
Luke McShane <lukejsmcshane@gmail.com>
2024-07-22 15:01:03 +0100

    created an overriding method for a basic calculator and commented appropriately

--- 00_CodingExercises/03_PolymorphismPractiseContinued.cs
+++ 00_CodingExercises/03_PolymorphismPractiseContinued.cs
@@ -5,16 +5,24 @@ class PolymorphismPractiseContinued
   public void Entry()
   {
     List<int> numbers = new List<int> { 1, 4, 6, 12, -12, 5, 32, -23, 5 };
-    bool shallAddPositiveOnly = true;
+    bool shallAddPositiveOnly = false;
 
-    if (shallAddPositiveOnly)
-    {
-      System.Console.WriteLine(new PositiveNumbersSumCalculator().Calculate(numbers));
-    }
-    else
-    {
-      System.Console.WriteLine(new NumbersSumCalculator().Calculate(numbers));
-    }
+    NumbersSumCalculator calculator = shallAddPositiveOnly == true ?
+                          new PositiveNumbersSumCalculator() :
+                          new NumbersSumCalculator();
+
+    int sum = calculator.Calculate(numbers);
+
+    System.Console.WriteLine($"Sum is: {sum}");
+
+    //   if (shallAddPositiveOnly)
+    //   {
+    //     System.Console.WriteLine(new PositiveNumbersSumCalculator().Calculate(numbers));
+    //   }
+    //   else
+    //   {
+    //     System.Console.WriteLine(new NumbersSumCalculator().Calculate(numbers));
+    //   }
   }
 
 }
@@ -31,11 +39,15 @@ class NumbersSumCalculator
     return sum;
   }
 
+  // In the base class, all number shall be added to the sum
   protected virtual bool ShallBeAdded(int number) => true;
 }
 
+// Adding this derived class allows us to define what type of number should be added in the calculator. This helps make code more maintainable as
+// we can add as many derived classes as we'd like to determine different criteria of what number should be added.
 class PositiveNumbersSumCalculator : NumbersSumCalculator
 {
+  // In this derived class, we overried the base class method to ensure that only positive numbers are being added.
   protected override bool ShallBeAdded(int number)
   {
     if (number > 0) return true;
