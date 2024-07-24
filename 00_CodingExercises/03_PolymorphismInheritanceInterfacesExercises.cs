c2cccb7364573152bef21e7d1cc0cd68689af09f
Luke McShane <lukejsmcshane@gmail.com>
2024-07-24 15:02:08 +0100

    Added an abstract method in a base class and overriden it in derived classes. Also created a file which uses abstract methods and an abstract class to demonstrate knowledge/skill

--- 00_CodingExercises/03_PolymorphismInheritanceInterfacesExercises.cs
+++ 00_CodingExercises/03_PolymorphismInheritanceInterfacesExercises.cs
@@ -26,11 +26,15 @@ public class PolymorphismInheritanceInterfacesExerices
     // Here we are downcasting, which is converting a base type to a derived type, which is created here.
     // This statement is saying "If the ingredient variable is of type Cheddar, then enter the if statement
     // creating a Cheddar object called cheddar1. This object will only be accessible within this statement.
-    // We could explicitly cast this instead by saying: "Cheddar cheddar1 = Cheddar(ingredient);"
+    // We could explicitly cast this instead by saying: "Cheddar cheddar1 = (Cheddar)ingredient;"
     if (ingredient is Cheddar cheddar1)
     {
       System.Console.WriteLine("Ingredient is cheddar: " + cheddar1);
     }
+
+    Cheddar cheddar2 = (Cheddar)ingredient; // If this conversion fails we will get a runtime error
+    Cheddar cheddar3 = ingredient as Cheddar; // If this conversion fails the value will be null, so is safer.
+                                              // Because of this we can only use the 'as' operator to cast to nullable types.
 
     // System.Console.WriteLine(pizza.Describe());
 
@@ -61,7 +65,8 @@ public class Pizza
 // All of these concrete types can be used wherever the generic concept is needed (in the ingredients list).
 // Inheritence enables us to create new classes that reuse, extend, and modify the behaviour defined in other classes.
 // The base class is the class whose members are inherited. The derived classes are those that inherit these members.
-public class Ingredient
+public abstract class Ingredient
+// The 'abstract' modifier makes it so the class cannot be instantiated. They only serve as base classes for other more concrete types.
 {
   public virtual string Name { get; init; } = "Some ingredient";
 
@@ -74,12 +79,20 @@ public class Ingredient
 
   public int PriceIfExtraTopping { get; }
 
+  // An abstract method *MUST* be overriden in the derived classes - it is not optional like a virtual method might be.
+  // And because the class it belongs to is abstract, it means this method specifically will never be directly called as
+  // we will never have an Ingredient object, and so we can leave the definition of the method blank.
+  // All abstract methods are therefore implicitly virtual.
+  public abstract void Prepare();
+
   public string PublicMethod() => "This is a PUBLIC method meaning all derived classes and non-derived classes can access it.";
   private string PrivateMethod() => "This is a PRIVATE method meaning it can only be access within the base class, i.e. this class.";
   protected string ProtectedMethod() => "This is a PROTECTED method meaning it can be accessed within the base *and* derived classes.";
 }
 
-public class Cheese : Ingredient
+// We can skip overriding the 'Prepare' abstract method in the Cheese class because this class is abstract, meaning that it will never
+// be instantiated.
+public abstract class Cheese : Ingredient
 {
   public Cheese(int priceIfExtraTopping) : base(priceIfExtraTopping)
   {
@@ -125,4 +138,16 @@ class Mozzarella : Cheese
   {
   }
   public override string Name => $"{base.Name}, more specifically, a mozzarella cheese.";
+
+    public override void Prepare()
+    {
+        throw new NotImplementedException();
+    }
 }
+
+/*
+VIRTUAL vs ABSTRACT Methods
+Virtual methods must have an implementation, and may or may not be overriden.
+
+Abstract methods cannot have an implementation, and must be overrident.
+*/
