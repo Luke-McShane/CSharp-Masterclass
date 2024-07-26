dc4f5e77722743c2752fbe108028c18bd761be21
Luke McShane <lukejsmcshane@gmail.com>
2024-07-26 20:54:56 +0100

    Commented code for clarity

--- 00_CodingExercises/ListExtensionExercise.cs
+++ 00_CodingExercises/ListExtensionExercise.cs
@@ -2,8 +2,11 @@
 {
   public static class ExtendInt
   {
+    // Extend the List class using the 'this' keyword in the parameter list.
     public static List<int> TakeEverySecond(this List<int> ints)
     {
+      // Create temporary list and iterate through each item, checking if it the second (and so on) item in the list using the modulo operator.
+      // Also check if the item is already in the list to be returned.
       List<int> res = new List<int>();
       for (int i = 0; i < ints.Count; ++i)
       {
