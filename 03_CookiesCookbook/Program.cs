f0606677b84022a3c2c45feae0cf0e391179a86c
Luke McShane <lukejsmcshane@gmail.com>
2024-07-28 18:35:38 +0100

    Began an assignment/project based on creating a cookie cookbook where the user can create cookie recipes and save them to a .json or .txt file and the program will read from these files. This project will utilise abstract/virtual classes, enums, casting, alongside various other skills learned up to this point

---
+++ 03_CookiesCookbook/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Runtime.InteropServices.Marshalling;
+using System.Security.Cryptography;
+
+var newline = Environment.NewLine;
+
+Console.WriteLine("Create a new cookie recipe! Available ingredients are:");
+Console.WriteLine(@$"1. Wheat flour{newline}2. Cocounut flour{newline}3. Butter{newline}4. Chocolate 
+                     5. Sugar{newline}6. Cardamon{newline}7. Cinnamon{newline}8. Cocoa powder");
+
+
+public virtual class Ingredient
+{
+
+}
+
+
+public class Recipe
+{
+
+  public void Create()
+  {
+    bool addIngredients = true;
+    Ingredients ingredient;
+    do
+    {
+      Console.WriteLine("Add an ingredient by its ID or type anything else if finished.");
+      var input = Console.ReadKey().ToString();
+      if (int.TryParse(input, out int result) && result <= 8 && result >= 1)
+      {
+        ingredient = (Ingredients)result - 1;
+      }
+    } while (addIngredients);
+  }
+}
+
+public enum Ingredients
+{
+  Wheat_flour,
+  Coconut_flour,
+  Butter,
+  Chocolate,
+  Sugar,
+  Cardamon,
+  Cinnamon,
+  Cocoa_powder
+
+}
