e7374800c35f441e20a1592b0385a8ece9b33497
Luke McShane <lukejsmcshane@gmail.com>
2024-07-29 11:04:39 +0100

    create abstract class and begin creating child/deriving classes

--- 03_CookiesCookbook/Program.cs
+++ 03_CookiesCookbook/Program.cs
@@ -10,9 +10,19 @@ Console.WriteLine(@$"1. Wheat flour{newline}2. Cocounut flour{newline}3. Butter{
                      5. Sugar{newline}6. Cardamon{newline}7. Cinnamon{newline}8. Cocoa powder");
 
 
-public virtual class Ingredient
+public abstract class Ingredient
 {
+  public abstract int Id { get; }
+  public abstract string Name { get; }
+  public abstract string PreparationInstructions { get; }
 
+}
+
+public class WheatFlour : Ingredient
+{
+  public override int Id { get => 1; }
+  public override string Name { get => "Wheat flour"; }
+  public override string PreparationInstructions { get => "Sieve. Add to other ingredients."; }
 }
 
 
