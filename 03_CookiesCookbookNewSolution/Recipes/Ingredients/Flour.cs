9f841e1e9c343f8a7c9028c9a542235dade06bc3
Luke McShane <lukejsmcshane@gmail.com>
2024-08-02 14:57:18 +0100

    Created appropriate abstract parent classes and used polymorphism correctly

---
+++ 03_CookiesCookbookNewSolution/Recipes/Ingredients/Flour.cs
@@ -0,0 +1,7 @@
+namespace CookieCookbook.Recipes.Ingredients;
+
+public abstract class Flour : Ingredient
+{
+  public override string PreparationInstructions =>
+      $"Sieve. {base.PreparationInstructions}";
+}
---
+++ 03_CookiesCookbookNewSolution/Recipes/Recipe.cs
@@ -0,0 +1,15 @@
+﻿namespace CookieCookbook.Recipes;
+
+public class Recipe
+{
+  // Angled brackets indicate that a generic is used to encapsulate some type. A generic is something that can store
+  // multiple types. Here the generic is holding the Ingredient type.
+
+  // The IEnumerable interface allows us to iterate from a collection or a data source. All collection types inherit from this interface.
+  public IEnumerable<Ingredient> Ingredients { get; }
+
+  public Recipe(IEnumerable<Ingredient> ingredients)
+  {
+    Ingredients = ingredients;
+  }
+}
