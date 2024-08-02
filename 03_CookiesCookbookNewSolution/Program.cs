8a54eb978849ff65832774f38e968fbd8d8eb79f
Luke McShane <lukejsmcshane@gmail.com>
2024-08-02 14:22:46 +0100

    Made use of dependency injection and the dependency inversion principle and made extensive comments explaining my code

--- 03_CookiesCookbookNewSolution/Program.cs
+++ 03_CookiesCookbookNewSolution/Program.cs
@@ -3,7 +3,9 @@
 // mapping out all the classes, methods, folders, etc., of the project before you jump into coding the details.
 
 
-var cookiesRecipeApp = new CookiesRecipeApp();
+var cookiesRecipeApp = new CookiesRecipeApp(
+  new RecipesRepository(),
+  new RecipesConsoleUserInteraction());
 cookiesRecipeApp.Run();
 
 // Coupling is the amount of interdependence between classes, a measure of how closely they are connected.
@@ -20,17 +22,20 @@ public class CookiesRecipeApp
   // We want to have instaces of these two other classes here to separate responsibilities. We don't want to have any direct user interaction
   // or reading or writing to text files within this class because that would break the Single Responsibility Principle.
   // This object will handle all the reading and writing to the text files.
-  private readonly RecipesRepository _recipesRepository;
+  private readonly IRecipesRepository _recipesRepository;
   // This object will handle all the user interaction.
   private readonly IRecipesUserInteraction _recipesUserInteraction;
 
+  // Dependency Injection: The class is given the dependencies it needs; it doesn't create them itself.
+  // Here we are shown some dependencies passed (injected) into the constructor, which is an example of constructor dependency injection,
+  // as opposed to creating all the dependencies by hard coding them in the class.
   public CookiesRecipeApp(
-    RecipesRepository recipesRepository,
-    RecipesConsoleUserInteraction recipesConsoleUserInteraction
+    IRecipesRepository recipesRepository,
+    IRecipesUserInteraction recipesUserInteraction
   )
   {
     _recipesRepository = recipesRepository;
-    _recipesUserInteraction = recipesConsoleUserInteraction;
+    _recipesUserInteraction = recipesUserInteraction;
   }
 
 
@@ -41,31 +46,34 @@ public class CookiesRecipeApp
 
     _recipesUserInteraction.PromptToCreateRecipe();
 
-    var ingredients = _recipesConsoleUserInteraction.ReadIngredientsFromUser();
+    var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
 
     if (ingredients.Count > 0)
     {
       var recipes = new Recipe(ingredients);
       allRecipes.Add(recipes);
       _recipesRepository.Write(filePath, allRecipes);
-      _recipesConsoleUserInteraction.ShowMessage("Recipe added:");
-      _recipesConsoleUserInteraction.ShowMessage(recipe.ToString());
+      _recipesUserInteraction.ShowMessage("Recipe added:");
+      _recipesUserInteraction.ShowMessage(recipe.ToString());
     }
     else
     {
-      _recipesConsoleUserInteraction.ShowMessage(
+      _recipesUserInteraction.ShowMessage(
         "No ingredients have been selected. " +
         "Recipe will not be saved.");
     }
 
-    _recipesConsoleUserInteraction.Exit();
+    _recipesUserInteraction.Exit();
   }
 }
 
-internal interface IRecipesUserInteraction
+public interface IRecipesUserInteraction
 {
   public void ShowMessage(string message);
   public void Exit();
+  object ReadIngredientsFromUser();
+  void PrintExistingRecipes(object allRecipes);
+  void PromptToCreateRecipe();
 }
 
 public class RecipesConsoleUserInteraction : IRecipesUserInteraction
@@ -81,6 +89,11 @@ public class RecipesConsoleUserInteraction : IRecipesUserInteraction
   }
 }
 
-public class RecipesRepository
+public interface IRecipesRepository
+{
+  object Read(object filePath);
+}
+
+public class RecipesRepository : IRecipesRepository
 {
 }
