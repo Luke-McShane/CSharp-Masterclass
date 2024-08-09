4b3a35fce462c762dbe6a94f3a293bafda5a1140
Luke McShane <lukejsmcshane@gmail.com>
2024-08-09 10:51:12 +0100

    More specific exceptions. Wrapping an exception within another exception.

--- 04_Exceptions/Exceptions_1.cs
+++ 04_Exceptions/Exceptions_1.cs
@@ -16,7 +16,39 @@ public static class Exceptions_1
     // 1. We cannot handle the invalid input in any reasonable way.
     // 2. The fault of the invalid input lies on the user of the method.
     // So here it makes sense to throw an exception, because this method should be only allowed to be passed collections that contain at least one item.
-    throw new Exception("Collection cannot be empty.");
+
+    // We throw the InvalidOperationException here because it is an invalid operation to seek the first element of an empty collection.
+    // We could also throw an IndexOutOfRangeException.
+    throw new InvalidOperationException("Collection cannot be empty.");
+  }
+
+  // If we want our code to compile, but haven't yet finished a method, we can throw the NotImplementedException.
+  bool CheckIfContains(int number, int[] numbers)
+  {
+    throw new NotImplementedException("Not yet implemented. Do not use.")
+  }
+
+  bool IsFirstElementPositive(IEnumerable<int> numbers)
+  {
+    try
+    {
+      var firstElement = GetFirstElement(numbers);
+      return firstElement > 0;
+    }
+    // Here we are catching the InvalidOperationException that was thrown from the GetFirstElement method.
+    catch (InvalidOperationException ex)
+    {
+      System.Console.WriteLine("The collection is empty. " +
+        "Exception mesage: " + ex);
+    }
+    // If the GetFirstElement throws a NullReferenceException, we can catch that and wrap the exception in our own
+    // ArgumentNullException that we throw and wrap the original exception within by passing it as an argument.
+    // The difference is that the NullReferenceException states merely that something is null, whereas the ArgumentNullException
+    // specifies that it is the argument that is null.
+    catch (NullReferenceException ex)
+    {
+      throw new ArgumentNullException("The collection cannot be null. ", ex);
+    }
   }
 }
 
@@ -34,7 +66,7 @@ public class Person
     }
     if (yearOfBirth < 1900 || yearOfBirth > DateTime.Now.Year)
     {
-      throw new ArgumentException("Please enter a valid year of birth. " +
+      throw new ArgumentOutOfRangeException("Please enter a valid year of birth. " +
         "The year of birth must be between 1900 and the current year.");
     }
 
@@ -42,3 +74,8 @@ public class Person
     YearOfBirth = yearOfBirth;
   }
 }
+
+/*
+EXCEPTIONS
+Remember that it is nearly always a bad idea to catch the Exception type, as we always want to be more specific than this. 
+*/
