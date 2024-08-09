192a72f0a1264483c7c57740497dc6744dfbb5dd
Luke McShane <lukejsmcshane@gmail.com>
2024-08-09 09:51:59 +0100

    Demonstrated use of more specific catch statements and explained usage.

--- 04_Exceptions/Program.cs
+++ 04_Exceptions/Program.cs
@@ -8,15 +8,30 @@ string input = Console.ReadLine();
 try
 {
   int userInt = ParseStringToInt(input);
-  System.Console.WriteLine("Successfully converted your input to an integer.");
+  int result = 10 / input;
+  System.Console.WriteLine($"10 / {input} = {result}.");
 }
 // If any code throws an exception in the try block, the catch block will be executed.
 // We can pass the exception through as an argument by adding the 'Exception' type followed by the variable name in the parameter list.
-// We iedally want to be more specific than just stating a generic Exception, but this works well for our case.
-catch (Exception e)
+// We iedally want to be more specific than just stating a generic Exception, as we can see below.
+catch (FormatException ex)
 {
   // Here we print the message of the exception to the console to give the user more information about what happened.
-  System.Console.WriteLine("An exception was thrown. Exception message: " + e.Message);
+  System.Console.WriteLine("Wrong format: cannot parse a string to an int. " +
+    "Exception message: " + ex.Message);
+}
+// We want to catch the exceptions from most specific to most generic, otherwise if we catch the 'Exception' type first, every exception will meet
+// this condition and we won't enter any more specific catch statement.
+catch (DivideByZeroException ex)
+{
+  System.Console.WriteLine("Division by zero is an invalid operation. " +
+    "Exception message: " + ex.Message);
+}
+// If the more generic catch statement comes before more specific catch statements, the code will simply not compile.
+catch (Exception ex)
+{
+  System.Console.WriteLine("Unknown exception occurred. " +
+    "Exception message: " + ex.Message);
 }
 // The code in the finally block will always be executed, no matter if there was an exception or not.
 // We don't need to add a finally block, and it is usually used to free up resources that we opened in the try block, such as a connection to a database.
