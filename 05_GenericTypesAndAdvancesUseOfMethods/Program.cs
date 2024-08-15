02ae89700672268442d9a874c8ff038d90b600c7
Luke McShane <lukejsmcshane@gmail.com>
2024-08-15 15:50:03 +0100

    made generic tuple with two parameters, and shown use of built-in Tuple type

--- 05_GenericTypesAndAdvancesUseOfMethods/Program.cs
+++ 05_GenericTypesAndAdvancesUseOfMethods/Program.cs
@@ -45,7 +45,20 @@ var pairOfDates = new Pair<DateTime>(new DateTime(2023, 12, 9), new DateTime(202
 
 PairOfInts minAndMax = GetMinAndMax(new List<int> { 4, 63, 54, 8, 17, 34, 27 });
 
+// Here is an example of making the most of the generic class we created to create objects with different data types in them.
+// But what if we want a tuple with more than two parameters?
+var tuple1 = new TupleExample<string, bool>("222", false);
+var tuple2 = new TupleExample<DateTime, float>(new DateTime(2014, 12, 31), 12.01f);
+var tuple3 = new TupleExample<decimal, int>(23.42m, 245);
+
+// We can use C#'s built in type, Tuple, to have as many parameters as we'd like.
+var builtInTuple1 = new Tuple<int, bool, string>(124, true, "example");
+
 Console.WriteLine($"Smallest number: {minAndMax.First}, Largest number: {minAndMax.Second}");
+
+// This method takes a collection of ints and rtturns the smallest and largest from the list as a PairOfInts object.
+// As you can see, this is restrictive because it can only take a collection of ints and can only return a 'PairOfInts' object,
+// but what if we wanted to return two strings or dates.
 PairOfInts GetMinAndMax(IEnumerable<int> input)
 {
   if (!input.Any())
@@ -68,6 +81,32 @@ PairOfInts GetMinAndMax(IEnumerable<int> input)
   }
   return new PairOfInts(min, max);
 }
+
+TupleExample<int, int> GetMinAndMaxTuple(IEnumerable<int> input)
+{
+  if (!input.Any())
+  {
+    throw new InvalidOperationException("The collection cannot be empty.");
+  }
+
+  int min = input.First();
+  int max = input.First();
+  foreach (int num in input)
+  {
+    if (num > max)
+    {
+      max = num;
+    }
+    if (num < min)
+    {
+      min = num;
+    }
+  }
+  return new TupleExample<int, int>(min, max);
+}
+
+
+
 
 Console.Read();
 
@@ -166,6 +205,19 @@ public class PairOfInts
   public int Second { get; }
 
   public PairOfInts(int first, int second)
+  {
+    First = first;
+    Second = second;
+  }
+}
+
+// Here is an example of a tuple we make, which takes two types as its parameters.
+public class TupleExample<T1, T2>
+{
+  public T1 First { get; }
+  public T2 Second { get; }
+
+  public TupleExample(T1 first, T2 second)
   {
     First = first;
     Second = second;
