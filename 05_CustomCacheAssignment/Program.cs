2b406cb462a8817bfcddb166f5f7b0ecb09f7358
Luke McShane <lukejsmcshane@gmail.com>
2024-09-02 16:25:41 +0100

    Boilerplate for caching assignment

---
+++ 05_CustomCacheAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿IDataDownloader dataDownloader = new SlowDataDownloader();
+
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+
+Console.ReadKey();
+
+public interface IDataDownloader
+{
+  TReturnType DownloadData<TKey, TReturnType>(TKey resourceId);
+}
+
+public class SlowDataDownloader<TKey> : IDataDownloader
+{
+  var dict = new Dictionary<
+  public TReturnType DownloadData<TKey, TReturnType>(TKey resourceId)
+  {
+    Thread.Sleep(1000);
+    return $"Some data for {resourceId}";
+  }
+}
