52ad8ae5b0830cd7c0ae7da477a01f71420338c8
Luke McShane <lukejsmcshane@gmail.com>
2024-09-02 16:54:35 +0100

    Beginning of assignment

--- 05_CustomCacheAssignment/Program.cs
+++ 05_CustomCacheAssignment/Program.cs
@@ -1,4 +1,6 @@
-﻿IDataDownloader dataDownloader = new SlowDataDownloader();
+﻿using System.Net.Http.Headers;
+
+IDataDownloader dataDownloader = new SlowDataDownloader<string, string>();
 
 Console.WriteLine(dataDownloader.DownloadData("id1"));
 Console.WriteLine(dataDownloader.DownloadData("id2"));
@@ -15,12 +17,26 @@ public interface IDataDownloader
   TReturnType DownloadData<TKey, TReturnType>(TKey resourceId);
 }
 
-public class SlowDataDownloader<TKey> : IDataDownloader
+public class Cache<TKey, TData>
 {
-  var dict = new Dictionary<
-  public TReturnType DownloadData<TKey, TReturnType>(TKey resourceId)
+  private readonly Dictionary<TKey, TData> _dict = new();
+  public TData FetchData(TKey key, TData data)
   {
+    if (!_dict.ContainsKey(key)) _dict.Add(key, data);
+    return _dict[key];
+  }
+}
+
+public class SlowDataDownloader<TKey, TVal> : IDataDownloader
+{
+  public TVal DownloadData(TKey resourceId)
+  {
+    if (!_dict.ContainsKey(resourceId)) _dict.Add(resourceId, ");
     Thread.Sleep(1000);
-    return $"Some data for {resourceId}";
+  }
+
+  public TReturnType DownloadData<TKey1, TReturnType>(TKey1 resourceId)
+  {
+    throw new NotImplementedException();
   }
 }
