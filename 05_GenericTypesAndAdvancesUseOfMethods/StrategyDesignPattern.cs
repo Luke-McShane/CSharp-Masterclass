beb1d92a017e7cb8d63b8642b8647df3bc17e776
Luke McShane <lukejsmcshane@gmail.com>
2024-09-02 13:07:06 +0100

    Implemented the Strategy design pattern so the code follows the open-closed principle

--- 05_GenericTypesAndAdvancesUseOfMethods/StrategyDesignPattern.cs
+++ 05_GenericTypesAndAdvancesUseOfMethods/StrategyDesignPattern.cs
@@ -1,5 +1,23 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
+/*
+OPEN-CLOSED PRINCIPLE (SOLID PRINCIPLE #2)
+
+The open-closed principle states that modules, classes, and functions should opened for extension but closed for modification.
+This states that we should create our code in such a way that its behaviour can be changed by adding new code, not changing existing code.
+
+Reasons we don't want to modify existing code:
+- The code works as expected, meaning no surprises for developers who have grown used to the code.
+- The code has already been tested manually by developers and automatically through unit testing.
+- If the code is part of a library, then by not modifying it we won't need to release a new version of the code to those using the library.
+*/
+
+/*
+STRATEGY DESIGN PATTERN
+
+The strategy design pattern allows us to define a family of algorithms that perform some tasks. The concrete strategy can be selected at runtime.
+*/
 namespace _05_GenericTypesAndAdvancesUseOfMethods;
 
 public static class StrategyDesignPattern
@@ -14,9 +32,11 @@ public static class StrategyDesignPattern
     Positive");
 
     var userInput = Console.ReadLine();
-    List<int> result = NumbersFilter.FilterBy(userInput, numbers);
+    var filteringStrategySelector = new FilteringStrategySelector();
+    var filteringStrategy = filteringStrategySelector.Select(userInput);
+    var filterResult = new NumbersFilter().FilterBy(filteringStrategy, numbers);
 
-    Print(result);
+    Print(filterResult);
   }
 
   private static void Print(IEnumerable<int> numbers)
@@ -25,25 +45,11 @@ public static class StrategyDesignPattern
   }
 }
 
-public static class NumbersFilter
-{
-  public static List<int> FilterBy(string filteringType, List<int> numbers)
-  {
-    switch (filteringType)
-    {
-      case "Odd":
-        return SelectByFilter(numbers, x => x % 2 == 1);
-      case "Even":
-        return SelectByFilter(numbers, x => x % 2 == 0);
-      case "Positive":
-        return SelectByFilter(numbers, x => x > 10);
-      default:
-        throw new NotSupportedException(
-          $"{filteringType} is not a valid filter.");
-    }
-  }
 
-  private static List<int> SelectByFilter(List<int> numbers, Func<int, bool> predicate)
+
+public class NumbersFilter
+{
+  public List<int> FilterBy(Func<int, bool> predicate, List<int> numbers)
   {
     var result = new List<int>();
     foreach (var num in numbers)
@@ -51,5 +57,21 @@ public static class NumbersFilter
       if (predicate(num)) result.Add(num);
     }
     return result;
+  }
+}
+
+public class FilteringStrategySelector
+{
+  private readonly Dictionary<string, Func<int, bool>> _filteringStrategies = new Dictionary<string, Func<int, bool>>
+  {
+    {"Odd", x => x % 2 == 1},
+    {"Even", x => x % 2 == 0},
+    {"Positive", x => x > 0}
+  };
+  
+  public Func<int, bool> Select(string filteringType)
+  {
+    if (!_filteringStrategies.ContainsKey(filteringType)) throw new NotSupportedException($"{filteringType} is not a valid filter.");
+    return _filteringStrategies[filteringType];
   }
 }
